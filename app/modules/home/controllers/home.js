418c296691fb6a2d4a8529c6b38999fdb68ed525
Grégory Houllier <gregory.houllier@niji.fr>
2014-07-06 20:11:11 +0200

    Ajout du controller home

---
+++ app/modules/home/controllers/home.js
@@ -0,0 +1,8 @@
+HomeController.$inject = ['$scope', '$log', 'trace'];
+function HomeController($scope, $log, trace) {
+  trace(arguments);
+  $scope.debug = 'debug';
+}
+
+ng.module('velocity.home')
+  .controller('HomeController', HomeController);
