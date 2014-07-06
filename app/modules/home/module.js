1ff25786620c4edf6788408cf7fddd4a45d50545
Grégory Houllier <gregory.houllier@niji.fr>
2014-07-06 11:35:28 +0200

    Test ui-router

--- app/modules/home/module.js
+++ app/modules/home/module.js
@@ -1,1 +1,23 @@
-var module = ng.module('velocity.home', []);
+ng.module('velocity.home', ['ui.router'])
+  .config(['$stateProvider', '$urlRouterProvider', velocityHomeModuleConfig]);
+
+/**
+ * Configure Home Module
+ * @param  {Object} $stateProvider
+ * @param  {Oject} $urlRouterProvider
+ * @return {void}
+ */
+function velocityHomeModuleConfig($stateProvider, $urlRouterProvider) {
+  console.log(arguments.callee.name, arguments);
+  $urlRouterProvider.otherwise('/home');
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'modules/home/partials/home.html'
+    })
+    .state('poke', {
+      url: '/poke',
+      template: '<velocity-hour></velocity-hour>'
+    });
+}
