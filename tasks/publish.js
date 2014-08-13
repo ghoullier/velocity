ba68537a0b59b01624f649c737b6edaa97bccb0b
ghoullier <gregory.houllier@gmail.com>
2014-08-13 09:41:04 +0200

    Update publish task to support package.json repository url

--- tasks/publish.js
+++ tasks/publish.js
@@ -1,12 +1,13 @@
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
+var package = require('../package');
 var paths = require('./paths');
 
-module.exports = function(){
+module.exports = function() {
   // https://github.com/rowoot/gulp-gh-pages/issues/26
   return gulp.src([paths.dist.root + '**/*.*'])
     .pipe(ghPages({
-      remoteUrl : 'https://github.com/ghoullier/velocity.git',
+      remoteUrl : package.repository.url,
       branch : 'gh-pages',
       cacheDir : __dirname + '/../.publish/'
     }))
