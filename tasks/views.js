9f8e6a4adadb911ef1603b7cdfb9c4431879b7c7
ghoullier <gregory.houllier@gmail.com>
2014-08-23 10:36:49 +0200

    Add htmlmin in task view

--- tasks/views.js
+++ tasks/views.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var refresh = require('gulp-livereload');
+var htmlmin = require('gulp-htmlmin');
 var lrserver = require('./live-reload');
 var paths = require('./paths');
 
@@ -7,12 +8,20 @@ var paths = require('./paths');
 module.exports = function() {
   // Get our index.html
   gulp.src(paths.sources.mainHtml)
+    // Will minimify html
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
     // And put it in the dist folder
     .pipe(gulp.dest(paths.dist.root))
     .pipe(refresh(lrserver));
 
   // Any other view files from app/views
   gulp.src(paths.sources.views)
+    // Will minimify html
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
     // Will be put in the dist/views folder
     .pipe(gulp.dest(paths.dist.views))
     .pipe(refresh(lrserver));
