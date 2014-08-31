786c7dfabf6b7a708adb5733feb7aff2ccb76ae1
ghoullier <gregory.houllier@gmail.com>
2014-08-31 18:13:24 +0200

    Update bike stations detail page

--- app/scripts/modules/view/controller/bike-station-detail.js
+++ app/scripts/modules/view/controller/bike-station-detail.js
@@ -7,25 +7,33 @@ function BikeStationDetailController($scope, $routeParams, BikeStationsViewData,
   $scope.station = {};
   $scope.map = {};
   $scope.options = {
-    center: new google.maps.LatLng(48.111, -1.695),
     zoom: 15,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   $scope.markers = [];
   BikeStationsViewData.getBikeStationById($routeParams.id).then(function (station) {
-    $scope.station = station;
     var position = new google.maps.LatLng(station.latitude, station.longitude);
+    $scope.station = station;
     $scope.map.panTo(position);
     $scope.markers.push(new google.maps.Marker({
       map: $scope.map,
-      position: position
+      position: position,
+      icon: 'https://maps.gstatic.com/mapfiles/ms2/micons/red-dot.png'
     }));
   });
-  Geolocation.getCurrentPosition().then(function (geoposition) {
-    var coords = geoposition.coords;
-    var center = new google.maps.LatLng(coords.latitude, coords.longitude);
-    $scope.map.setCenter(center);
-  });
+  function geolocalize() {
+    Geolocation.getCurrentPosition().then(function onGetCurrentPosition(geoposition) {
+      var coords = geoposition.coords;
+      var center = new google.maps.LatLng(coords.latitude, coords.longitude);
+      $scope.map.setCenter(center);
+      $scope.markers.push(new google.maps.Marker({
+        map: $scope.map,
+        position: center,
+        icon: 'https://maps.gstatic.com/mapfiles/ms2/micons/blue-dot.png'
+      }));
+    });
+  }
+  this.geolocalize = geolocalize;
 }
 
 module.exports = BikeStationDetailController;
